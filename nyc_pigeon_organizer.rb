294be634696064d2f1e218a3f26de0d387377368
Jordan Panasewicz <jbpanasewicz@gmail.com>
2020-10-12 18:56:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  data.each_with_object ({}) do | (key, value), accum |
+    
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !accum[name]
+          accum[name] = {}
+        end
+        if !accum[name][key]
+          accum[name][key] = []
+        end
+          accum[name][key].push(inner_key.to_s)
+      end 
+    end  
+  end
+#  binding.pry
 end
