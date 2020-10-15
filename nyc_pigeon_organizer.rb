cad0e01b14357ea46716c480f800f5508a3cfc67
Allen Richard Lea <allenlea2007@gmail.com>
2020-10-15 15:33:02 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+pigeon_list = {}
+data.each {|cgl, atr|
+atr.each{|key, value|
+value.each{|name|
+
+if !pigeon_list[name]
+pigeon_list[name] = {}
+end
+
+if !pigeon_list[name][cgl]
+pigeon_list[name][cgl] = []
+end
+
+pigeon_list[name][cgl].push("#{key}")
+
+
+}
+}
+}
+pigeon_list
 end
